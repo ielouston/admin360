764d391f872e25bc44b9a9bf6b982697713a3f8b
ielouston <alan.sosa.g@hotmail.com>
2018-04-01 20:55:45 -0500

    models updated

--- app/Provider.php
+++ app/Provider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace admin360;
 
 use Illuminate\Database\Eloquent\Model;
 use admin360\Stock;
--- app/User.php
+++ app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace admin360;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
